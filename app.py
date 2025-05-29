3f60ed0dd0d566b1abeaa4531948c111344e977a
Arlisoh <michael@mccabemedia.com>
2025-05-29 18:16:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,6 @@ def analyze():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))  # <- this line fixes it
-    app.run(debug=True, host='0.0.0.0', port=port)
+app.run(debug=False, host='0.0.0.0', port=port, use_reloader=False)
+
 
